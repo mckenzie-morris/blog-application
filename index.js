68937377bbd5fd7d4a571cd87ea21eb1292af763
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-22 14:38:01 -0700

    in progress 4/22/24

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+/* 
+-'CREATE NEW POST' button positioned at the top ==> click on button,
+ expands/opens two text editors/input boxes with 'POST' button and 'CLEAR' button
+
+ -1st input box is for title
+
+ -'CLEAR' button ==> deletes everything within box
+
+ -'POST' button ==> will close input boxes, add the post to the list of posts, along with
+ the date it was created, and to the top of the list
+*/
+
+/*
+-Click on a post (will show title and date created in list), have option to edit or
+delete from list
+*/
